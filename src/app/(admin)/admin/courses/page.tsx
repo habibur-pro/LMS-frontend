1d04ffe1397c1982055d637c012e852960959885
Habibur Rahman <va.habibur@gmail.com>
2025-08-20 16:11:21 +0600

    fix: fix build error

--- src/app/(admin)/admin/courses/page.tsx
+++ src/app/(admin)/admin/courses/page.tsx
@@ -32,7 +32,7 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
-export const getCourseCounts = (courses: ICourse[]) => {
+const getCourseCounts = (courses: ICourse[]) => {
   return courses?.reduce(
     (acc, course) => {
       switch (course.status) {
